e69e9d8e6096e7ef996784affddcaa4f2f7f0895
Alexander Mendez <bluespace@gmail.com>
2014-12-24 12:00:56 -0800

    added temperature

--- bin/battest.sh
+++ bin/battest.sh
@@ -1,2 +1,3 @@
 date "+%s" >> ~/data/.batlog.dat
 /usr/sbin/ioreg -l | egrep "CycleCount|Capacity" >> ~/data/.batlog.dat
+echo "$(date '+%s') $(/usr/local/bin/osx-cpu-temp)" >> ~/data/.templog.chargino.dat
