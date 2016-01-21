fe28273c85f2c2b4b360f45d77270240662d9b9a
Alexander Mendez <bluespace@gmail.com>
2016-01-21 13:49:33 -0500

    adding image capture

---
+++ bin/hourly.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# date
+HOST="$(hostname -s)"
+IMGFILE="$HOME/Dropbox/Photos/wakeup/wakeup_${HOST}_$(date '+%s').jpg"
+
+#echo $IMGFILE $HOST
+
+# does not allow for width/height
+/usr/local/bin/imagesnap $IMGFILE
+#/Users/ajmendez/Dropbox/bin/isightcapture -w 1280 -h 1024 -n 24 $IMGFILE >> /tmp/wakeup.log 2>&1
+
