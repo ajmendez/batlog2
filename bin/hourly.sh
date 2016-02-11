d05dd73719e0866e3dbb8596ebab9b662d4b2df6
Alexander Mendez <bluespace@gmail.com>
2016-02-11 12:16:45 -0500

    update hourly

--- bin/hourly.sh
+++ bin/hourly.sh
@@ -7,6 +7,6 @@ IMGFILE="$HOME/Dropbox/Photos/wakeup/wakeup_${HOST}_$(date '+%s').jpg"
 #echo $IMGFILE $HOST
 
 # does not allow for width/height
-/usr/local/bin/imagesnap $IMGFILE
+/usr/local/bin/imagesnap $IMGFILE >> /tmp/wakeup.log 2>&1
 #/Users/ajmendez/Dropbox/bin/isightcapture -w 1280 -h 1024 -n 24 $IMGFILE >> /tmp/wakeup.log 2>&1
 
